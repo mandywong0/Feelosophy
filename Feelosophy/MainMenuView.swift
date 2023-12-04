de5dab3e0e114bc898bc280b931205a5c8fd5ee2
Avery Chen <91500567+averychen35@users.noreply.github.com>
2023-12-04 09:56:53 -0800

    bare bones main menu view

--- Feelosophy/MainMenuView.swift
+++ Feelosophy/MainMenuView.swift
@@ -11,7 +11,21 @@ struct MainMenuView: View {
     @Environment(\.modelContext) private var modelContext
     
     var body: some View {
-        Text("Hello")
+        VStack{
+            Spacer()
+            Text("Welcome to Feelosophy!").font(.title)
+            Spacer()
+            Text("This app was created with the mental health of students in mind. Whether you're feeling on top of the world, or down in the dumps, this app will provide you with actionable suggestions on what to do based on how you are feeling. Here at Feelosophy, we believe that it's crucial to stay in touch with your own emotions.  ").frame(width:300).multilineTextAlignment(.center)
+            Spacer()
+            Text("We understand that this app may not be able to replace or provide professional medical services. If you need more help, please utilize these mental health resources:").frame(width:300).multilineTextAlignment(.center)
+            Spacer()
+            Text("- Urgent Drop In Counseling: (510) 642-9494")
+            Text("- National suicide prevention hotline: Dial 988")
+            Text("- Path to Care: Dial (510) 643-2005")
+            Spacer()
+            
+            
+        }
     }
 }
 
